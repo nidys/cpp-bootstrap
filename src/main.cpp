826703a315664e87758e740a17eb3822086ccb79
nidys <nidys0000@gmail.com>
2017-12-17 12:20:31 +0100

    clean makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include "hello.h"
+#include <thread>
+#include<iostream>
+void call_from_thread(int tid)
+{
+  std::cout << "hi tid = " << tid << std::endl;
+}
+
+const int num_th = 10;
 
 int main()
 {
+  std::thread ths[num_th];
+
+  for(int i = 0; i < num_th; i++)
+  {
+    ths[i] = std::thread(call_from_thread, i + 1);
+  }
+  
+  for(int i = 0; i < num_th; i++)
+  {
+    ths[i].join();
+  }
   printHello();
 
   return 0;
